b33973646589b976e138cb6118080e59c83b1a8f
Nobel Elan <nobelelan@gmail.com>
2022-08-14 20:26:47 +0600

    added circular image

--- app/src/main/java/com/example/moretalk/User.kt
+++ app/src/main/java/com/example/moretalk/User.kt
@@ -4,12 +4,14 @@ class User {
     var name: String? = null
     var email: String? = null
     var uid: String? = null
+//    var image: String? = null
 
     constructor(){}
 
-    constructor(name: String?, email: String?, uid: String?){
+    constructor(name: String?, email: String?, uid: String? ){
         this.name = name
         this.email = email
         this.uid = uid
+//        this.image = image
     }
 }
